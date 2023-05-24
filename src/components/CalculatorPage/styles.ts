b2a101eea84f237f6e019fa4f30d53ec7e7c7d8a
unknown <ruanlucenaramos@gmail.com>
2023-05-24 12:52:06 -0300

    add page 2 styles

--- src/components/CalculatorPage/styles.ts
+++ src/components/CalculatorPage/styles.ts
@@ -1,114 +1,79 @@
 import styled from "styled-components";
-
+import bg from "../../assets/bg.svg";
 
 export const StyledInputs = styled.div`
-  margin: auto;
   padding: 10px;
   width: 90vw;
-  height: 45vh;
+  bottom: 50px;
+  border-radius: 20px;
+  margin: auto;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
-  background: #E4E4E4;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
-  border-radius: 10px;
   position: relative;
-  top: -50px;
- 
-  
-  
+  background-color: #fff;
 
   > div {
-    position: relative;
-    left: 10px;
     flex-basis: 48%;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    
   }
-label {
+  label {
     font-size: 18px;
     margin-bottom: 3px;
     margin-top: 5px;
     font-weight: 500;
-}
+  }
   input {
     width: 90%;
-    height: 50px;
-    background: #FFFFFF;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
+    margin-bottom: 40px;
+    font-size: 16px;
+    padding: 16px;
     border: none;
-    display: flex;
-    margin-bottom: 68px;
-    font-size: 16px;
-    text-align: center;
-    
-    
+    border-bottom: 1px solid #000000;
+    background-color: transparent;
   }
-
-  button {
-    position: absolute;
-    bottom: 10px;
-    right: 15px;
-    width: 81px;
-    height: 46px;
-    background: #96F196;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
-    border-radius: 5px;
-    font-weight: 400;
-    font-size: 16px;
-  }
-  
 `;
 
-export const StyledDisplaykW = styled.div`
-background: #96F196;
-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
-border-radius: 5px;
- width: max-content;
- height: 110px;
- display: flex;
- align-items: center;
- padding: 15px;
- margin-bottom: 40px;
- font-weight: 400;
- font-size: 15px;
- text-align: center;
- h3 {
-    margin-bottom: 10px;
-}
+export const StyledButtonInpt = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  button {
+    border-radius: 5px;
+    padding: 20px;
+    width: 50%;
+    font-size: 18px;
+    font-weight: bold;
+    background-color: #299d80;
+    color: #fff;
+  }
+`;
 
-p{
-    font-size:1.3em
-}
-`
- 
+export const StyledDisplayWrapper = styled.div`
+  background-image: url(${bg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  display: flex;
+  justify-content: space-around;
 
- export const StyledDisplayValor = styled.div`
- background: #96F196;
-box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
-border-radius: 5px;
-width: 160px;
-height: 110px;
-display: flex;
-justify-content: center;
-padding: 15px;
-margin-bottom: 40px;
-font-weight: 400;
-font-size: 15px;
-text-align: center;
-h3 {
-    margin-bottom: 10px;
-}
+  border-radius: 20px;
+  margin: 20px;
+  padding: 30px 0;
+  height: 200px;
+  margin-bottom: 20%;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
 
-p{font-size:1.7em}
-`
- 
- export const StyledDisplayWrapper = styled.div`
- display: flex;
- justify-content: space-around;
- margin-bottom: 15%;
-`
+  p {
+    font-size: 18px;
+    line-height: 2;
+  }
 
+  .values {
+    width: 125px;
+    height: max-content;
+    display: flex;
+    justify-content: space-around;
+  }
+`;
